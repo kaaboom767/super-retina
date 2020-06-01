19c778c2d34772f732b790bbf8f014bf1b1e64e2
kaaboom767 <66246397+kaaboom767@users.noreply.github.com>
2020-06-01 11:19:55 +0200

    removed fixed intensities

--- Pilot3/Full Experiment/experiment_multiple_intensities.m
+++ Pilot3/Full Experiment/experiment_multiple_intensities.m
@@ -94,7 +94,7 @@ end
 
 
 %% Directory stuff for data storage
-WorkingDir = ['C:\Users\kaaboom\Hyperion Cloud\ETH\Master Thesis\Data\Pilot3\1.3-WP3-' p.SubjectsNumber '\']; %set directory for windows to save data
+WorkingDir = [p.SubjectsNumber '\']; %set directory for windows to save data
 WorkingDir = [WorkingDir{:}];
 WorkingDir = convertCharsToStrings(WorkingDir);
 
@@ -156,8 +156,8 @@ p.VisualThreshold = visualThreshold(2,1);
 
 
 %% linearise the screen contrast
-load('C:\Users\kaaboom\Hyperion Cloud\ETH\Master Thesis\Scripts\Alain\Beta folder\Calibration\normalGamma.mat');
-load('C:\Users\kaaboom\Hyperion Cloud\ETH\Master Thesis\Scripts\Alain\Beta folder\Calibration\gammaTableSonyCorrect.mat'); %%% Screen calibration
+load('normalGamma.mat');
+load('gammaTableSonyCorrect.mat'); %%% Screen calibration
 Screen('LoadNormalizedGammaTable', 0, gammaTable);
 
 
